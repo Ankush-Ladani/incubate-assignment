86bf708b2dce473b60a4df00ad7de72182f0715e
Ankush Ladani <astarladani@gmail.com>
2023-08-10 11:24:48 +0530

    Done with Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,28 @@
 import React from "react";
+import Logo from "../images/moon.png";
 
 const Header = () => {
   return (
-    <nav>
+    <nav className="flex justify-between">
       <div>
-        <img
-          data-testid="dummyLogo"
-          src="https://blog.incubyte.co/images/logo.png"
-          class="w-32"
-          alt=""
-        />
+        <a href="#">
+          <img
+            data-testid="dummyLogo"
+            src="https://blog.incubyte.co/images/logo.png"
+            class="w-32"
+            alt=""
+          />
+        </a>
       </div>
-      <div></div>
+      <div className="flex gap-x-4 items-cente">
+        <img src={Logo} className="w-8" alt="" />
+        <span className="bg-blue-900 px-7 font-semibold rounded-full py-2 text-white">
+          <button>
+            <a href="https://www.incubyte.co/lets-get-started">LET'S TALK</a>
+          </button>
+        </span>
+        <p className="flex items-center font-medium">home</p>
+      </div>
     </nav>
   );
 };
